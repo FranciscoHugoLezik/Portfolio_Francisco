ef2a69fe33b020cb39f3a031a715acd8be0de0c1
Juani Solari <jignaciosolari@gmail.com>
2021-11-18 16:18:30 -0300

    transitions: finished

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import './navBar.css'
 
 const NavBar = ({ scrolling }) => {
+    
+    const toTheTop = () => {
+        window.scrollTo({top: 0, left: 0, behavior:"smooth"})
+    }
+    
     return (
-        <nav className={`navbar ${scrolling > 20 ? "scrolled" : null} `}>
+        <nav className={`navbar ${scrolling > 100 ? "scrolled" : null} `}>
             <div className="navbar-logo">
-                go UP
+               <span className={`${scrolling > 250 ? "navbar-span" : null}`} onClick={toTheTop}>J S</span> 
             </div>
         </nav>
     )
