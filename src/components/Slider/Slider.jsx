69477c7367c2b7aba114c3c393e7dff16bf13587
Juani Solari <jignaciosolari@gmail.com>
2021-11-19 21:36:38 -0300

    responsive updates // starting carousel

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './slider.css'
+
+const Slider = () => {
+    return (
+        <div className="container-carousel">
+            <div className="container-slaider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left"></div>
+                        <div className="rigth"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Slider
