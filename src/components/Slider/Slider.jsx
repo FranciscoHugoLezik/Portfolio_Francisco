4bea2849a469a5b11adec7832178b5dd2375dc0c
Juani Solari <jignaciosolari@gmail.com>
2021-11-23 23:36:14 -0300

    slider done

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { AiFillGithub } from "react-icons/ai";
 import { HiOutlineLink } from "react-icons/hi";
-import pF from '../../media/proyectos/WaveMusicCut.png'
+import data from './data'
+import { MdArrowForwardIos } from "react-icons/md";
+
 import './slider.css'
 
 const Slider = () => {
     return (
-        <div className="container-carousel">
+        <div className="container-carousel" id="id">
             <div className="container-slaider">
-                <div className="containerR">
+                { data.map(e => (
+                    <div className="containerR">
                     <div className="item">
                         <div className="left">
                             <div className="lefTContainer">
-                                <h2>Tittle</h2>
-                                <p>Lorem ipsum jjjjjdolor, sit amet consectetur adipisicing elit. Numquam asperiores sed exercitationem esse atque doloremque tenetur ut dolor vitae, </p>
+                                <h2>{e.tittle}</h2>
+                                <p>{e.desc}</p>
                                 <div className="btns-container">
                                 <a href="https://www.linkedin.com/" target="_blank"><HiOutlineLink className="link-icon" /></a>
                                 <a href="https://www.github.com/" target="_blank"><AiFillGithub className="github-icons" /></a>
@@ -21,13 +24,14 @@ const Slider = () => {
                             </div>
                         </div>
                         <div className="rigth">
-                            <img src={pF} alt="" />
+                            <img src={e.img} alt="" />
                         </div>
                     </div>
                 </div>
+                ))}
             </div>
-            <img src="" alt="" />
-            <img src="" alt="" />
+            <span className="arrow-left"><MdArrowForwardIos/></span>
+            <span className="arrow-rigth"><MdArrowForwardIos/></span>
         </div>
     )
 }
---
+++ src/components/Slider/data.js
@@ -0,0 +1,26 @@
+import img1 from '../../media/proyectos/WaveMusicCut.png'
+import img2 from '../../media/proyectos/PI.png'
+import img3 from '../../media/proyectos/Rez.png'
+
+const data = [
+    {
+        id: 1,
+        tittle: "Wave Music",
+        desc: "Lorem ipsum jjjjjdolor, sit amet consectetur adipisicing elit. Numquam asperiores sed exercitationem esse atque doloremque tenetur ut dolor vitae, ",
+        img: img1 
+    },
+     {
+        id: 2,
+        tittle: "PI - Videogames",
+        desc: "Lorem ipsum jjjjjdolor, sit amet consectetur adipisicing elit. Numquam asperiores sed exercitationem esse atque doloremque tenetur ut dolor vitae, ",
+        img: img2
+    },
+    {
+        id: 3,
+        tittle: "Weather App",
+        desc: "Lorem ipsum jjjjjdolor, sit amet consectetur adipisicing elit. Numquam asperiores sed exercitationem esse atque doloremque tenetur ut dolor vitae, ",
+        img: img3
+    } 
+]
+
+export default data
