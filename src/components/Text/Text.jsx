a3cdfc5fd667967fcef64bbbd6a750842fc5f5fd
Juani Solari <jignaciosolari@gmail.com>
2021-11-26 17:49:20 -0300

    fixing

--- src/components/Text/Text.jsx
+++ src/components/Text/Text.jsx
@@ -10,7 +10,7 @@ const Text = ({scrolling}) => {
         <div className="info-container">
             <div className="text">
                 <h1>Trabajemos en equipo y hagamos algo único. </h1>
-                <h1 className={`contact-me ${scrolling > 1600 ? 'contact-scroll' : null}`}>Contactame :)</h1>
+                <h1 className={`contact-me ${scrolling > 1400 ? 'contact-scroll' : null}`}>Contactame :)</h1>
             </div>
         </div>
     )
